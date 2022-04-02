68f6ca33fb2499e3f4824247e339783c6370a827
1mmdfederal <102861370+1mmdfederal@users.noreply.github.com>
2022-04-02 17:12:15 +0430

    unistall.bash

--- uninstall.sh
+++ uninstall.sh
@@ -14,8 +14,8 @@ echo -e "                 \ \'      "
 echo -e "                  \ \     "
 echo -e "                   '-'    \e[1;97m "
 echo ""
-echo -e "\e[1;91m [+] Telegram: \e[1;92mMR_x_Hacke"
-echo -e "\e[1;91m [+] Github: \e[1;92mHOSEIN-MRX\e[1;97m"
+echo -e "\e[1;91m [+] Telegram: \e[1;cr_federal"
+echo -e "\e[1;91m [+] Github: \e[1;1mmd.federal\e[1;97m"
 echo ""
 read -p " Does you want to Uninstall Parrot Shell?(Yes/No) : " input
 
